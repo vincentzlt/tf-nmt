0ea9afa3ec1cbd2387cde4377e7220184188f30f
vincentzlt <vincentzlt@gmail.com>
2017-12-19 01:50:35 +0900

    create train data and train script

---
+++ training_scripts_20171216/test_script/test_script.sh
@@ -0,0 +1,35 @@
+
+NMT_ROOT=/clwork/vincentzlt/tf-nmt
+cd ${NMT_ROOT}
+MODEL_ROOT=/work/vincentzlt/tf-nmt/model-WMTsetting-20171216
+MODEL=test_script
+if [ ! -e ${MODEL_ROOT}/${MODEL} ]; then
+mkdir ${MODEL_ROOT}/${MODEL}
+fi
+
+echo 'working on ' `pwd` 
+echo 'save model to ' ${MODEL_ROOT}/${MODEL}
+
+
+DATA_ROOT=`pwd`/nmt/testdata/reverse-order
+DATA_SPLIT=
+SHARE_VOCAB=false
+if [ ${SHARE_VOCAB} = true ]; then
+    SHARE_VOCAB_DIR=share
+else
+    SHARE_VOCAB_DIR=noshare
+fi
+
+
+CUDA_VISIBLE_DEVICES=2 python -m nmt.nmt \
+    --src=source \
+    --tgt=target \
+    --share_vocab=${SHARE_VOCAB} \
+    --vocab_prefix=${DATA_ROOT}/train/vocab \
+    --train_prefix=${DATA_ROOT}/train/data \
+    --dev_prefix=${DATA_ROOT}/dev/data \
+    --test_prefix=${DATA_ROOT}/test/data \
+    --out_dir=${MODEL_ROOT}/${MODEL} \
+    --hparams_path=/clwork/vincentzlt/tf-nmt/nmt/standard_hparams/wmt16-batch64.json \
+    --override_loaded_hparams | tee ${MODEL_ROOT}/${MODEL}/log_`date "+%Y-%m-%d_%H_%M_%S"`
+
---
+++ training_scripts_20171216/test_script/test_script_char.sh
@@ -0,0 +1,37 @@
+
+NMT_ROOT=/clwork/vincentzlt/tf-nmt
+cd ${NMT_ROOT}
+MODEL_ROOT=/work/vincentzlt/tf-nmt/model-WMTsetting-20171218
+MODEL=test_script_char
+if [ ! -e ${MODEL_ROOT}/${MODEL} ]; then
+mkdir ${MODEL_ROOT}/${MODEL}
+fi
+
+echo 'working on ' `pwd` 
+echo 'save model to ' ${MODEL_ROOT}/${MODEL}
+
+
+DATA_ROOT=`pwd`/nmt/testdata/mini-data/
+DATA_SPLIT=char
+SHARE_VOCAB=false
+if [ ${SHARE_VOCAB} = true ]; then
+    SHARE_VOCAB_DIR=share
+else
+    SHARE_VOCAB_DIR=noshare
+fi
+
+
+CUDA_VISIBLE_DEVICES=6 python3 -m nmt.nmt  \
+    --src=jp \
+    --tgt=cn\
+    --metrics=char_bleu,kytea_bleu \
+    --text_format=${DATA_SPLIT} \
+    --share_vocab=${SHARE_VOCAB} \
+    --vocab_prefix=${DATA_ROOT}/${DATA_SPLIT}/vocab \
+    --train_prefix=${DATA_ROOT}/${DATA_SPLIT}/train \
+    --dev_prefix=${DATA_ROOT}/${DATA_SPLIT}/dev \
+    --test_prefix=${DATA_ROOT}/${DATA_SPLIT}/test \
+    --out_dir=${MODEL_ROOT}/${MODEL} \
+    --hparams_path=/clwork/vincentzlt/tf-nmt/nmt/standard_hparams/wmt16-batch64.json \
+    --override_loaded_hparams | tee ${MODEL_ROOT}/${MODEL}/log_`date "+%Y-%m-%d_%H_%M_%S"`
+
