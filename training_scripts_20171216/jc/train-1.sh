8d1587d5d11b4011617556c5be2a325009e96dd4
vincentzlt <vincentzlt@gmail.com>
2017-12-19 17:25:15 +0900

    add example training scripts and modify the hparams

--- training_scripts_20171216/jc/train-1.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/zsh
-
-source ~/.profile
-export CUDA_VISIBLE_DEVICES=1
-
-for f in *char* *CompSp4k* *MecabJieba*;do
-
-    ./${f}
-done
--- training_scripts_20171216/jc/train-2.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/zsh
-
-source ~/.profile
-export CUDA_VISIBLE_DEVICES=3
-
-for f in *sp4k* *StrokeSp4k*;do
-
-    ./${f}
-done
