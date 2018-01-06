86440a0495cdf1ad5415df76934c6f752b883383
vincentzlt <vincentzlt@gmail.com>
2018-01-06 20:54:29 +0900

    new training script for 5 seed

---
+++ train_scripts_5_seeds/jc_char-6030-4228_noshare_64_bi-LSTM_2.sh
@@ -0,0 +1,78 @@
+MODEL=jc_char-6030-4228_noshare_64_bi-LSTM_2
+CORPUS=char # ('char','subword','mecab-jieba','comp','stroke','mecab-jieba_comp')
+SUBWORD_OPTION=
+TEXT_FORMAT=char
+MODEL_ARCHITECTURE=LSTM
+SHARE_VOCAB=false
+RAMDOM_SEED=50507
+CUDA_VISIBLE_DEVICES=3
+
+NMT_ROOT=/clwork/vincentzlt/tf-nmt
+cd ${NMT_ROOT}
+MODEL_ROOT=/clwork/vincentzlt/tf-nmt/model-5_seeds
+
+echo 'working on ' $(pwd)
+echo 'save model to ' ${MODEL_ROOT}/${MODEL}
+mkdir ${MODEL_ROOT}/${MODEL} -p
+
+DATA_ROOT=/clwork/vincentzlt/tf-nmt/data_2
+
+
+if [ ${CORPUS} = char ]; then
+	DATA_PREFIX=${DATA_ROOT}/${CORPUS}
+
+elif [ /${CORPUS} = subword ]; then
+	SUBWORD_TYPE=bpe        # ('bpe','spm')
+	SUBWORD_VOCAB_SIZE=4000 # (4000 8000 16000 32000 64000)
+	DATA_PREFIX=${DATA_ROOT}/${CORPUS}/${SUBWORD_TYPE}/${SUBWORD_VOCAB_SIZE}
+
+	SUBWORD_OPTION=spm
+
+elif [ ${CORPUS} = mecab-jieba ]; then
+	DATA_PREFIX=${DATA_ROOT}/${CORPUS}
+
+elif [ ${CORPUS} = comp ]; then
+	SUBWORD_TYPE=spm        # ('bpe','spm')
+	SUBWORD_VOCAB_SIZE=8000 # (1000 2000 4000)
+	DATA_PREFIX=${DATA_ROOT}/${CORPUS}/${SUBWORD_TYPE}/${SUBWORD_VOCAB_SIZE}
+
+	SUBWORD_OPTION=spm
+	TEXT_FORMAT=comp
+
+elif [ ${CORPUS} = stroke ]; then
+	SUBWORD_TYPE=bpe        # ('bpe','spm')
+	SUBWORD_VOCAB_SIZE=2000 # (1000 2000 4000)
+	DATA_PREFIX=${DATA_ROOT}/${CORPUS}/${SUBWORD_TYPE}/${SUBWORD_VOCAB_SIZE}
+
+	SUBWORD_OPTION=spm
+	TEXT_FORMAT=stroke
+
+fi
+
+if [ ${SHARE_VOCAB} = false ]; then
+	VOCAB_PREFIX=vocab
+elif [ ${SHARE_VOCAB} = true ]; then
+	VOCAB_PREFIX=vocab.share
+fi
+
+if [ ${MODEL_ARCHITECTURE} = LSTM ]; then
+    HPARAM_PATH=./LSTM.json
+elif [ ${MODEL_ARCHITECTURE} = GNMT ]; then
+    HPARAM_PATH=./GNMT.json
+fi
+
+CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} python3 -m nmt.nmt \
+	--src=jp \
+	--tgt=cn \
+	--metrics=char_bleu,kytea_bleu \
+	--text_format=${TEXT_FORMAT} \
+	--subword_option=${SUBWORD_OPTION} \
+	--share_vocab=${SHARE_VOCAB} \
+	--vocab_prefix=${DATA_PREFIX}/${VOCAB_PREFIX} \
+	--train_prefix=${DATA_PREFIX}/train \
+	--dev_prefix=${DATA_PREFIX}/dev \
+	--test_prefix=${DATA_PREFIX}/test \
+	--out_dir=${MODEL_ROOT}/${MODEL} \
+	--hparams_path=${HPARAM_PATH} \
+    --random_seed=${RAMDOM_SEED} \
+	--override_loaded_hparams | tee ${MODEL_ROOT}/${MODEL}/log_$(date "+%Y-%m-%d_%H_%M_%S")
